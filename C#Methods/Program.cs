caefac04b75f78ddf4e244bb58a50a4a827879c9
laithalfa <laith.alfaloujeh@gmail.com>
2020-12-20 22:38:10 -0600

    Sucesfully created a method for Exercises

--- C#Methods/Program.cs
+++ C#Methods/Program.cs
@@ -6,42 +6,48 @@ namespace C_methods
     {
         static void Main(string[] args)
         {
-            MyMethod();
-             Console.WriteLine("Please input a number: ");
+           meetAlien();
+            Console.WriteLine("Please input a number: ");
             int num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please input a second number: ");
             int num2 = Convert.ToInt32(Console.ReadLine());
-
-            int result = ComparingIngeres(number1 , number2, number3);
-
-            Console.WriteLine(result);
+            ComparingIntegers(num1, num2);
         }
 
-        static void MyMethod()
+        static void meetAlien()
         {
-            Console.WriteLine("MyMethod works");
+            Random numberGen = new Random();
+
+            string name = "x-" + numberGen.Next(10, 999);
+            int age = numberGen.Next(10,999);
+            Console.WriteLine("Hi, I'm "+ name);
+            Console.WriteLine("I'm "+ age);
+            Console.WriteLine("OH, and I'm an alien");
+                       
         }
 
-
-        static void ComparingIngeres ()
+        static int ComparingIntegers(int num1, int num2)
         {
-            if (num1 > num2)
-            {
-                Console.WriteLine(num2 + ": is Larger");
-                Console.WriteLine(num1 + ": is Smaller");
-            }
-            else if(num1 < num2)
-            {
-              Console.WriteLine(num1 + ": is Larger");
-                Console.WriteLine(num2 + ": is Smaller"); 
-               
-            }
-            else
-            {
-               Console.WriteLine("These numbers are equal");
+            int largeValue = 0;
 
+            if (num1 < num2)
+            {
+                largeValue = num2;
+                Console.WriteLine(largeValue + ": is Larger");
 
+            }else if (num1 > num2)
+            {
+                largeValue = num1;
+                Console.WriteLine(largeValue + ": is Larger");
+
+            }else
+            {
+                Console.WriteLine("These numbers are equal");
             }
+
+            return largeValue;
+
         }
+        
     }
 }
